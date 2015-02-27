3198743ad7a4321e7cd180802042cb2910d020eb
Loren M. Kerr <loren@apportunities.la>
2015-02-26 18:49:28 -0600

    Init

---
+++ Vendoki.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vendoki.Web.MvcApplication" Language="C#" %>
---
+++ Vendoki.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vendoki.Web.Startup))]
+namespace Vendoki.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
